3f8f44a86d48df12a85c6d241bb22bb09bb91e57
Pedro Ribeiro <gearb8t@gmail.com>
2022-11-13 12:16:35 -0300

    Mostrar bolsas disponíveis

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ class Usuario:
 	def __init__(self):
 		# perguntar dados do usário
 		self.nome = input("Nome do usuário: ")
-		self.enem = input("Nota do ENEM: ")
+		self.enem = int(input("Nota do ENEM: "))
 
 		# calcular renda
 		pessoas = int(input("Quantas pessoas moram com você? "))
@@ -12,9 +12,24 @@ class Usuario:
 		self.renda = sum(salarios)/pessoas
 		print(f"A renda de sua família é de R${self.renda}.")
 
+		# calcular desconto
+		self.desconto = 0
+		if self.renda <= 1818:
+			self.desconto = 100
+		elif self.renda <= 2424:
+			self.desconto = 75
+		elif self.renda <= 3636:
+			self.desconto = 50
+		elif self.renda <= 6060:
+			self.desconto = 25
+
+		if self.desconto > 0:
+			print(f"De acordo com sua renda familiar, você pode concorrer à bolsas de {self.desconto}% de desconto.\n")
+
 
 class Bolsa:
-	def __init__(self, vagas, n_corte):
+	def __init__(self, desconto, vagas, n_corte):
+		self.desconto = desconto
 		self.vagas = vagas
 		self.n_corte = n_corte
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
 	usr = Usuario()
 
 	# mostrar faculdades disponíveis
+	print("As faculdades disponíveis são:")
 	i = 1
 	for facul in faculdades:
 		print(f"[{i}] Faculdade {facul.nome}")
@@ -18,7 +19,7 @@ def main():
 		if (escolha > 0 and escolha <= len(faculdades)):
 			break
 		else:
-			print("\nEste não é um número válido.")
+			print("\nDigite um número válido.")
 	facul = faculdades[escolha-1]
 
 	# escolher um curso
@@ -31,9 +32,14 @@ def main():
 			print("\nEste não é um número válido.")
 	curso = cursos[escolha-1]
 
-	print(f"Você se matriculou no curso de {curso.nome} na faculdade {facul.nome}.")
-
 	# mostrar bolsas disponíveis
+	print("\nDe acordo com sua renda familiar e nota do ENEM, você pode concorrer às seguintes bolsas:")
+	i = 1
+	for bolsa in curso.bolsas:
+		if (bolsa.desconto <= usr.desconto) and (bolsa.n_corte < usr.enem):
+			print(f"[{i}] Bolsa de {bolsa.desconto}% de desconto, com nota de corte de {bolsa.n_corte} e {bolsa.vagas} vagas.")
+		i = i+1
+
 	# informar usuario caso esteja concorrendo
 
 if __name__ == "__main__":
