17856c393fe8f74e53c465e19d005c827d7a807f
Pedro Ribeiro <gearb8t@gmail.com>
2022-11-24 17:58:55 -0300

    modificado main.py

--- main.py
+++ main.py
@@ -19,36 +19,35 @@ def main():
 			break
 		else:
 			print("\nDigite um número válido.")
+
+	# remover 1 pois listas começam no número 0
 	facul = faculdades[escolha-1]
 
 	# escolher um curso
 	facul.mostrarCursos()
 	while True:
 		escolha = int(input("\nEm qual curso você deseja se matricular? "))
-		if (escolha > 0 and escolha < len(facul.cursos)):
+		if (escolha > 0 and escolha <= len(facul.cursos)):
 			break
 		else:
 			print("\nEste não é um número válido.")
+
+	# remover 1 pois listas começam no número 0
 	usr.curso = cursos[escolha-1]
 
 	# calcular a qual bolsa o usuario pode concorrer
 	i = 1
 	for bolsa in usr.curso.bolsas:
-			if (bolsa.desconto == usr.desconto):
-				print("\nDe acordo com sua renda per capta familiar e nota do ENEM, você pode concorrer à seguinte bolsa:")
-				print(f"[{i}] Bolsa de {bolsa.desconto}% de desconto, com nota de corte de {bolsa.n_corte} e {bolsa.vagas} vagas.")
-			i = i+1
+		if (bolsa.desconto == usr.desconto):
+			usr.bolsa = bolsa
+			break
+		i = i+1
+	
+	if (usr.bolsa.n_corte <= usr.enem):
+		print("\nDe acordo com sua renda per capta familiar e nota do ENEM, você pode concorrer à seguinte bolsa:")
+		print(f"Bolsa de {usr.bolsa.desconto}% de desconto, com nota de corte de {usr.bolsa.n_corte} e {usr.bolsa.vagas} vagas.")
 	else:
-		if (bolsa.n_corte > usr.enem):
-			print("\nPorém, sua nota do ENEM é menor do que a nota de corte do curso. Você pode paticipar da lista de espera após o resultado.")
-
-	# informar usuario caso esteja concorrendo
+		print("\nSua nota do ENEM é menor do que a nota de corte do curso. Você pode paticipar da lista de espera após o resultado.")
 
 if __name__ == "__main__":
 	main()
-	def opcoes(self):
-		self.opcao = input("Deseja concorrer à bolsa? (SIM/NAO)")
-		if self.opcao == "SIM":
-			print("Parabéns! Você está concorrendo à bolsa. Aguarde a convocação no edital que será lançado.")
-		else: 
-			print("ENCERRADO")
